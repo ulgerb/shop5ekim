b22c5366d84a4f995576918207cbeda012ebb4f5
Berkay Ülger <berkayulger45920@gmail.com>
2022-12-22 01:45:14 +0300

    first load

---
+++ shop5ekim/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for shop5ekim project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop5ekim.settings')
+
+application = get_asgi_application()
